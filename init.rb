ee2397c85203c81af30e78e5c0a65b93d4114f0d
Gabriel Arjones <arjones@arjones.com>
2019-06-13 21:43:26 -0300

    Add demo files

---
+++ init.rb
@@ -0,0 +1,4 @@
+require 'autoproj/gitorious'
+Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
+Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
+
